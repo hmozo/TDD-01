c051f8b4521660b882ad741adeae0b39db5b284b
Hugo Mozo <jhmozo@hotmail.com>
2020-03-20 12:32:56 +0100

    create project

---
+++ tddJava-01/src/main/java/com/packtpublishing/tddjava/ch02friendships/Friendships.java
@@ -0,0 +1,10 @@
+package com.packtpublishing.tddjava.ch02friendships;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Friendships {
+
+
+}
---
+++ tddJava-01/src/test/java/com/packtpublishing/tddjava/TddJava01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.packtpublishing.tddjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddJava01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
